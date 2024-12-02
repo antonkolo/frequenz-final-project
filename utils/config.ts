679d8b771a71e407adc34272bae23f8d50e8f86f
Anton Kolo <kvanton7@gmail.com>
2024-12-02 13:20:48 +0100

    Edit postgres config

--- utils/config.ts
+++ utils/config.ts
@@ -6,10 +6,13 @@ export const postgresConfig = {
     ...postgres.camel,
     undefined: null,
   },
+
+  ssl: false,
 };
 
 export function setEnvironmentVariables() {
   if (process.env.NODE_ENV === 'production' || process.env.CI) {
+    postgresConfig.ssl = true;
     // Set standard environment variables for Postgres.js from
     // Vercel environment variables
     if (process.env.POSTGRES_URL) {
