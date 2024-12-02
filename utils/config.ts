00136c582ab77930c7f5b9d67564c2a42011060a
Anton Kolo <kvanton7@gmail.com>
2024-12-02 13:25:46 +0100

    Change postgres config

--- utils/config.ts
+++ utils/config.ts
@@ -1,18 +1,19 @@
 import { config } from 'dotenv-safe';
 import postgres from 'postgres';
 
+export const sslConfig = Boolean(process.env.POSTGRES_URL);
+
 export const postgresConfig = {
   transform: {
     ...postgres.camel,
     undefined: null,
   },
 
-  ssl: false,
+  sl: sslConfig,
 };
 
 export function setEnvironmentVariables() {
   if (process.env.NODE_ENV === 'production' || process.env.CI) {
-    postgresConfig.ssl = true;
     // Set standard environment variables for Postgres.js from
     // Vercel environment variables
     if (process.env.POSTGRES_URL) {
