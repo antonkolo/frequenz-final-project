c954b7b55fd5ffb1d95ed0f484a8d9b538127feb
Anton Kolo <kvanton7@gmail.com>
2024-12-02 13:30:28 +0100

    Edit ley config;

--- ley.config.js
+++ ley.config.js
@@ -3,11 +3,15 @@ import postgres from 'postgres';
 
 config();
 
+export const sslConfig = Boolean(process.env.POSTGRES_URL);
+
 const option = {
   trasform: {
     ...postgres.camel,
     undefined: null,
   },
+
+  ssl: sslConfig,
 };
 
 export default option;
